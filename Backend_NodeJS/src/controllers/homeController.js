ea15520a1bdc23c85ad9359d5b0f0c03c22f52e5
ngocduy1226 <lamngocduy2001@gmail.com>
2023-08-17 22:35:10 -0800

    Chuc nang CRUD nguoi dung

--- Backend_NodeJS/src/controllers/homeController.js
+++ Backend_NodeJS/src/controllers/homeController.js
@@ -1,28 +1,87 @@
-import db from '../models/index';
+import { response } from "express";
+import db from "../models/index";
+import UserService from "../services/CRUDService";
 
-let getHomePage = async (req,res) => {
-    //return res.send('hello controller');
-    try {
-        let data = await db.User.findAll();
-        // console.log('-------------------------------------')
-        // console.log(data);
-        // console.log('-------------------------------------')
-        return res.render('homePage.ejs', {
-            data: JSON.stringify(data)
-        });
-    }
-    catch(err){
-        console.log(err);
-    }
+let getHomePage = async (req, res) => {
+  //return res.send('hello controller');
+  try {
+    let data = await db.User.findAll();
+    // console.log('-------------------------------------')
+    // console.log(data);
+    // console.log('-------------------------------------')
+    return res.render("homePage.ejs", {
+      data: JSON.stringify(data),
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
+let getUser = async (req, res) => {
+  let data = await UserService.getAllUser();
+  console.log('---------------------------------');
+  console.log(data);
+  console.log('---------------------------------');
+  return res.render("user/getAllUser", {
+    dataTable: data
+  });
 
+};
+
+let getEditUser = async (req, res) => {
+  let userId = req.query.id;
+  if(userId) {
+    let userData = await UserService.getUserInfoById(userId);
+    
+    return res.render('user/editUser.ejs', {
+       user: userData,
+    })
+  } else {
+    return res.send('user not found');
+  }
+};
+
+let putUser = async (req,res ) => {
+    let data = req.body;
+    let allUsers = await UserService.updateUserData(data);
+    return res.render("user/getAllUser", {
+      dataTable: allUsers,
+    });
+  
 }
 
-let getUser =  (req, res) => {
-    return res.render('user/login.ejs');
+let deleteUser = async (req, res) => {
+  let id = req.query.id;
+  if(id) {
+     await UserService.deleteUserById(id);
+      return res.send('delete user done');
+  }
+   else {
+    return res.send('delete user failed');
+   }
+
+ 
 }
 
+let getUserSign = (req, res) => {
+  return res.render("user/sign.ejs");
+};
+let getUserLogin = (req, res) => {
+  return res.render("user/login.ejs");
+};
+
+let postUserSign = async (req, res) => {
+  let message = await UserService.createNewUser(req.body);
+  console.log(message);
+  return res.send("đã đăng ký thành công");
+};
 module.exports = {
-    getHomePage: getHomePage,
-    getUser: getUser,
-}
+  getHomePage: getHomePage,
+  getUser: getUser,
+  getEditUser: getEditUser,
+  deleteUser: deleteUser,
+  getUserLogin: getUserLogin,
+  getUserSign: getUserSign,
+  postUserSign: postUserSign,
+  putUser: putUser,
+};
--- Backend_NodeJS/src/route/web.js
+++ Backend_NodeJS/src/route/web.js
@@ -1,18 +1,33 @@
 import express from "express";
 
 import homeController from "../controllers/homeController";
+import userController from "../controllers/userController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
    router.get('/', homeController.getHomePage);
+
    router.get('/user', homeController.getUser);
-   
-   
-   router.get('/hehe', (req, res) => {
-      return res.send("hello world hehe");
-     });
+   router.get('/user/edit-user', homeController.getEditUser);
+   router.post('/user/put-user', homeController.putUser);
+
+   router.get('/user/delete-user', homeController.deleteUser);
+
+   router.get('/user/login', homeController.getUserLogin);
+   router.get('/user/sign', homeController.getUserSign);
+
+   router.post('/user/user-sign', homeController.postUserSign);
+
+   // router.get('/hehe', (req, res) => {
+   //    return res.send("hello world hehe");
+   //   });
+  
+   router.post('/api/login', userController.handleLogin);
+
    return app.use("/", router);
+
+
 }
 
 module.exports = initWebRoutes;
